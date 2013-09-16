d7c44cbe0652fc3363db0215a1f5b3d0c8e17cb2
RyanEggert <Ryan.Eggert@students.olin.edu>
2013-09-15 21:32:12 -0400

    Trying degrees
    Why do two servos short it out?

--- ServoPlay2.ino
+++ ServoPlay2.ino
@@ -1,7 +1,7 @@
 // ServoPlay2.ino
 #include "Servo.h"
 
-
+int Position=0;
 Servo Pan;
 Servo Tilt;
 
@@ -9,19 +9,22 @@ Servo Tilt;
 void setup()
 {
 	Pan.attach(9);			//Attaches Pan to pin 9
-	Tilt.attach(11);		//Attaches Tilt to pin 11
+	Tilt.attach(10);		//Attaches Tilt to pin 11
+	Serial.begin(9600);
 }
 
 void loop() 
 {
-	Pan.writeMicroseconds(1500);
-	Pan.writeMicroseconds(1500);
+	//Pan.writeMicroseconds(1000);
+	//Pan.writeMicroseconds(1000);
 
 //Where we could put pushbutton scan start
-	for(int PanPosition=1000; PanPosition<2000; PanPosition+=50)
+	for(int PanPosition=0; PanPosition<180; PanPosition+=10)
 	{
-		Pan.writeMicroseconds(PanPosition);
-		delay(500);
+		Pan.write(PanPosition);
+		Tilt.write(PanPosition);
+		delay(1000);
+
 	}
 
 }
