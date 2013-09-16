1e5b125fba4396775b3cca1fca9b046a60f4bbc8
RyanEggert <Ryan.Eggert@students.olin.edu>
2013-09-16 16:46:28 -0400

    Reformatted, added button, runonce
    Added RunOnce (similar to modes), which should make the Arduino perform
    the scan once--no infinite looping. Added pushbutton, to begin scanning
    upon a button HIGH (like bike light). Compiles successfully.

--- LIDAR/LIDAR.ino
+++ LIDAR/LIDAR.ino
@@ -11,62 +11,77 @@ int Distance3=0;
 int Distance4=0;
 int Distance5=0;
 int TenXDistance=0;			//Final Distance (has been averaged and multiplied by 10)
-int SensorPin=0;			//Pin the distance sensor is plugged into. MUST BE ANALOG INPUT
+int SensorPin=A0;			//Pin the distance sensor is plugged in to. MUST BE ANALOG INPUT
+int ButtonPin=13;				//Digital pin which the push button is plugged in to.
+int ButtonState=0;			//State (HIGH/LOW) of the push button
+int RunOnce=0;				//Will be used to make the for loops only run once
 Servo Pan;
 Servo Tilt;
 
 
 void setup()
 {
+	pinMode(ButtonPin,INPUT);
 	Pan.attach(9);			//Attaches Pan servo to pin 9
 	Tilt.attach(10);		//Attaches Tilt servo to pin 11
 	Serial.begin(9600);
-	Serial.print("A=[")		//Preparing the data to be written to the serial monitor in a format readable by MATLAB as a matrix, A.
+	Serial.print("A=[");	//Preparing the data to be written to the serial monitor in a format readable by MATLAB as a matrix, A.
 }
 
 void loop() 
 {
-	//Pan.writeMicroseconds(1000);
-	//Pan.writeMicroseconds(1000);
+	ButtonState=digitalRead(ButtonPin);
 
-//Where we could put pushbutton scan start
-	for(int PanAngle=10; PanAngle<170; PanAngle+=10)
+	if (ButtonState=HIGH)
 	{
-		Pan.write(PanAngle);
-
-		for(int TiltAngle=10; TiltAngle<170; TiltAngle+=10)
+		while (ButtonState=HIGH)
 		{
-			Tilt.write(TiltAngle);										//Tilt the sensor
-			delay(500);													//Give the servo time to reach its position
-			//Collect Data
-			Distance1=analogRead(SensorPin);
-			delay(20);													//Stop between readings
-			Distance2=analogRead(SensorPin);
-			delay(20);													//Stop between readings
-			Distance3=analogRead(SensorPin);
-			delay(20);													//Stop between readings
-			Distance4=analogRead(SensorPin);
-			delay(20);													//Stop between readings
-			Distance5=analogRead(SensorPin);
-
-			//Average the distance readings
-			TenXDistance=((Distance1 + Distance2 + Distance3 + Distance4 + Distance5)*10)/5;  //Distances multiplied by 10 to keep values integers. Will be divided out in MATLAB. This works because x/5, where x is an integer, will have a maximum of one decimal point.
-			
-			//Calculated outputted tilt angle
-
-				//Wait for finished mount
-
-			//Print formatted data to serial monitor.
-			Serial.print(PanAngle);
-			Serial.print(",");
-			Serial.print(TiltAngleOut);
-			Serial.print(",");
-			Serial.print(TenXDistance);
-			Serial.println(";");
+			ButtonState=digitalRead(ButtonPin);
 		}
-		
-
+		RunOnce++;
 	}
-
-}
+	
+	if (RunOnce==1)
+	{
+		for(int PanAngle=10; PanAngle<170; PanAngle+=10)
+		{
+			Pan.write(PanAngle);
+	
+			for(int TiltAngle=10; TiltAngle<170; TiltAngle+=10)
+			{
+					Tilt.write(TiltAngle);										//Tilt the sensor
+					delay(500);													//Give the servo time to reach its position
+					//Collect Data
+					Distance1=analogRead(SensorPin);
+					delay(20);													//Stop between readings
+					Distance2=analogRead(SensorPin);
+					delay(20);													//Stop between readings
+					Distance3=analogRead(SensorPin);
+					delay(20);													//Stop between readings
+					Distance4=analogRead(SensorPin);
+					delay(20);													//Stop between readings
+					Distance5=analogRead(SensorPin);
+	
+					//Average the distance readings
+					TenXDistance=((Distance1 + Distance2 + Distance3 + Distance4 + Distance5)*10)/5;  //Distances multiplied by 10 to keep values integers. Will be divided out in MATLAB. This works because x/5, where x is an integer, will have a maximum of one decimal point.
+					
+					//Calculated outputted tilt angle
+	
+						//Wait for finished mount
+	
+					//Print formatted data to serial monitor.
+					Serial.print(PanAngle);				// The data will be exported to matlab as a matrix, A.
+					Serial.print(",");					// The matrix will have the following form
+					Serial.print(TiltAngleOut);			//		PanAngle1	TiltAngle1	Distance1*10
+					Serial.print(",");					//		PanAngle2	TiltAngle2	Distance2*10
+					Serial.print(TenXDistance);			//			...			...			...
+					Serial.println(";");				// These should correspond to spherical coordinates (theta, phi, r) 
+				}
+				
+	
+			}
+			Serial.print("]");
+			RunOnce++;
+		}
+	}
 
